71df7b31c2667d0c5746d7c92ccce3f7fd073525
unknown <n02285069@.active.newpaltz.edu>
2013-09-09 16:16:22 -0400

    Final has some php Playground has new pages/changes for first project

--- Playground/contacts.php
+++ Playground/contacts.php
@@ -3,23 +3,13 @@
   <head>
     <title>Bootstrap 101 Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">   
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">    
   </head>
   <body>
-    <h1>This is the final</h1>
+  	
+ <h1>Contacts</h1>
     
-    <? 
-    	
-    	$msg= 'Hello ';
-		$name = 'Kurt';
-    	include 'something.php'; 
-    
-    
-    ?>
-    
-    <span class="label label-success"><?= $msg . $name ?> </span>
-
+   
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="//code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
--- Playground/index.php
+++ Playground/index.php
@@ -2,46 +2,16 @@
 <html>
   <head>
     <title>Bootstrap 101 Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="../../assets/js/html5shiv.js"></script>
-      <script src="../../assets/js/respond.min.js"></script>
-    <![endif]-->
-  
-  <style>
-  	.row div{
-  		background-color: #AACCCC;
-  	}
-  </style>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet" media="screen">    
   </head>
   <body>
-    <h1>This is the playground</h1>
+  	<?
+  		include 'navbar.php';
+	?>
+    <!--<h1>This is the playground</h1>-->
     
-    <div class="container">
-	<!-- Stack the columns on mobile by making one full-width and the other half-width -->
-	<div class="row">
-	  <div class="col-xs-12 col-md-8">.col-xs-12 col-md-8</div>
-	  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-	</div>
-	
-	<!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
-	<div class="row">
-	  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-	  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-	  <div class="col-xs-6 col-md-4">.col-xs-6 .col-md-4</div>
-	</div>
-	
-	<!-- Columns are always 50% wide, on mobile and desktop -->
-	<div class="row">
-	  <div class="col-xs-6">.col-xs-6</div>
-	  <div class="col-xs-6">.col-xs-6</div>
-	</div>
-	</div>
-
+   
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="//code.jquery.com/jquery.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
